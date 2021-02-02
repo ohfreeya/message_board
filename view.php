ef591effef590f1066a752d93ea4aba496853448
ohfreeya <student40807@gmail.com>
2021-02-02 16:31:28 +0800

    留言列表

--- view.php
+++ view.php
@@ -5,29 +5,27 @@
 </head>
 
 <body>
-    <center>
-        <a href="write.php">寫寫留言</a>
-        <?php
-        include("db.php");
-        /* 查詢欄位資料 */
-        $sql = "select * from boarddata order by name desc";  //從guestbook讀取資料並依no欄位做遞減排序
-        $result = mysqli_query($con, $sql);
-        /* 顯示資料庫資料 */
-        while (list($id, $name, $content, $datadate) = mysqli_fetch_row($result)) {
-            echo "<br>姓名:" . $name;
-            echo "<br>留言內容:" . nl2br($content);
-            echo "<br>留言時間:" . $datadate;
-        ?>
-            <a href="edit.php?id= <?php echo $id; ?>">修改</a>
-            <a href="delete.php?id= <?php echo $id; ?>">刪除</a>
-            <hr>
+    <a href="write.php">寫寫留言</a>
+    <?php
+    include("db.php");
+    /* 查詢欄位資料 */
+    $sql = "select * from boarddata order by name desc";  //從guestbook讀取資料並依no欄位做遞減排序
+    $result = mysqli_query($con, $sql);
+    /* 顯示資料庫資料 */
+    while (list($id, $name, $content, $datadate) = mysqli_fetch_row($result)) {
+        echo "<br>姓名:" . $name;
+        echo "<br>留言內容:" . nl2br($content);
+        echo "<br>留言時間:" . $datadate;
+    ?>
+        <a href="edit.php?id= <?php echo $id; ?>">修改</a>
+        <a href="delete.php?id= <?php echo $id; ?>">刪除</a>
+        <hr>
 
-        <?php
-        }
+    <?php
+    }
 
-        echo "共" . mysqli_num_rows($result) . "筆留言";
-        ?>
-    </center>
+    echo "共" . mysqli_num_rows($result) . "筆留言";
+    ?>
 </body>
 
 </html>
