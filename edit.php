5da822908b99b522e5bf6ecf70e2ddd115d80b97
ohfreeya <student40807@gmail.com>
2021-02-02 16:38:42 +0800

    取消center的使用

--- edit.php
+++ edit.php
@@ -14,14 +14,14 @@
     $result = mysqli_query($con, $query);
     list($id, $name, $content, $datadate) = mysqli_fetch_row($result);
     ?>
-    <center>
-        <form action="modification.php" method="POST">
-            暱稱 : <input type="text" value="<?php echo $name ?>" name="name"><br>
-            內容 : <textarea name="content" placeholder="<?php echo $content ?>" cols="30" rows="10"></textarea><br>
-            <input type="submit" value="submit">
-            <input type="reset" value="reset">
-        </form>
-    </center>
+
+    <form action="modification.php" method="POST">
+        暱稱 : <input type="text" value="<?php echo $name ?>" name="name"><br>
+        內容 : <textarea name="content" placeholder="<?php echo $content ?>" cols="30" rows="10"></textarea><br>
+        <input type="submit" value="submit">
+        <input type="reset" value="reset">
+    </form>
+    
 </body>
 
 </html> 
