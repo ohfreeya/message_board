730f51986087251fdbfd12ed95208327abe26182
ohfreeya <student40807@gmail.com>
2021-02-02 16:40:57 +0800

    取消center使用

--- write.php
+++ write.php
@@ -7,15 +7,13 @@
 </head>
 
 <body>
-    <center>
-        <a href="view.php">留言列表</a>
-        <form name="form1" action="add.php" method="post">
-            暱稱 : <input type="text" name="name"><br>
-            內容 : <textarea name="content" cols="30" rows="10"></textarea><br>
-            <input type="submit" value="submit">
-            <input type="reset" value="reset">
-        </form>
-    </center>
+    <a href="view.php">留言列表</a>
+    <form name="form1" action="add.php" method="post">
+        暱稱 : <input type="text" name="name"><br>
+        內容 : <textarea name="content" cols="30" rows="10"></textarea><br>
+        <input type="submit" value="submit">
+        <input type="reset" value="reset">
+    </form>
 </body> 
 
 </html>
